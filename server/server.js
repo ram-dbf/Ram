b7bea6082d17ab5a3d44912f59e132a98a9ae2c6
ram-dbf <ram@digitalbluefoam.com>
2022-07-06 14:19:51 +0530

    First commit with all files

---
+++ server/server.js
@@ -0,0 +1,47 @@
+//Import all required npms and load functions
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const randomColor = require('randomcolor');
+const createBoard = require('./create-board');
+const createCooldown = require('./create-cooldown');
+
+// Establish the local server
+const app = express();
+
+app.use(express.static(`${__dirname}/../client`));
+
+// Socket server side
+const server = http.createServer(app);
+const io = socketio(server);
+
+const { clear, getBoard, makeTurn } = createBoard(20);
+
+io.on('connection', (sock) => {
+  const color = randomColor();
+  const cooldown = createCooldown(2000);
+  sock.emit('board', getBoard());
+
+  sock.on('message', (text) => io.emit('message', text));
+  sock.on('turn', ({ x, y }) => {
+    if (cooldown()) {
+      const playerWon = makeTurn(x, y, color);
+      io.emit('turn', { x, y, color });
+
+      if (playerWon) {
+        sock.emit('message', 'You Won!');
+        io.emit('message', 'New Round');
+        clear();
+        io.emit('board');
+      }
+    }
+  });
+});
+
+server.on('error', (err) => {
+  console.error(err);
+});
+
+server.listen(8080, () => {
+  console.log('server is ready');
+});
